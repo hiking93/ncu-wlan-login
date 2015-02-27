1e388441b23d6d1154b0b7a99d10a63a7a721c49
Hiking <tom.xj.cheng@gmail.com>
2015-02-28 00:29:27 +0800

    Update .gitignore

---
+++ NCUWLogin/app/build/intermediates/classes/debug/com/devandroid/ncuwlogin/AboutActivity$$ViewInjector.java
@@ -0,0 +1,17 @@
+// Generated code from Butter Knife. Do not modify!
+package com.devandroid.ncuwlogin;
+
+import android.view.View;
+import butterknife.ButterKnife.Finder;
+
+public class AboutActivity$$ViewInjector {
+  public static void inject(Finder finder, final com.devandroid.ncuwlogin.AboutActivity target, Object source) {
+    View view;
+    view = finder.findRequiredView(source, 2131296319, "field 'mMainListView'");
+    target.mMainListView = (android.widget.ListView) view;
+  }
+
+  public static void reset(com.devandroid.ncuwlogin.AboutActivity target) {
+    target.mMainListView = null;
+  }
+}
