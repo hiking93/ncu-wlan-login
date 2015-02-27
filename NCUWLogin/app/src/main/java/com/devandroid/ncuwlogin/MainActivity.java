40d42ee8f056d12196517a34a77accb5e1b24d98
Hiking <tom.xj.cheng@gmail.com>
2015-02-27 23:29:41 +0800

    Design improvements, v2.1 release.

--- NCUWLogin/app/src/main/java/com/devandroid/ncuwlogin/MainActivity.java
+++ NCUWLogin/app/src/main/java/com/devandroid/ncuwlogin/MainActivity.java
@@ -1,17 +1,9 @@
 package com.devandroid.ncuwlogin;
 
-import com.devandroid.ncuwlogin.callbacks.Constant;
-import com.devandroid.ncuwlogin.callbacks.GeneralCallback;
-import com.devandroid.ncuwlogin.callbacks.Memory;
-import com.devandroid.ncuwlogin.libs.LoginHelper;
-
-import com.devandroid.ncuwlogin.R;
-
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,6 +11,14 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.devandroid.ncuwlogin.callbacks.Constant;
+import com.devandroid.ncuwlogin.callbacks.GeneralCallback;
+import com.devandroid.ncuwlogin.callbacks.Memory;
+import com.devandroid.ncuwlogin.libs.LoginHelper;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends ActionBarActivity implements OnClickListener {
 
@@ -49,10 +49,17 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 	private void setUpViews() {
 		mLoginButton.setOnClickListener(this);
 		mLogoutButton.setOnClickListener(this);
-		mUsernameEditText.setText(Memory.getString(this,
-				Constant.MEMORY_KEY_USER, ""));
-		mPasswordEditText.setText(Memory.getString(this,
-				Constant.MEMORY_KEY_PASSWORD, ""));
+		mUsernameEditText.setText(Memory.getString(this, Constant.MEMORY_KEY_USER, ""));
+		mPasswordEditText.setText(Memory.getString(this, Constant.MEMORY_KEY_PASSWORD, ""));
+		mPasswordEditText.setImeActionLabel(getText(R.string.ime_submit), KeyEvent.KEYCODE_ENTER);
+		mPasswordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				saveAndLogin();
+				return false;
+			}
+		});
 	}
 
 	@Override
@@ -65,12 +72,12 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
 		switch (id) {
-		case R.id.about:
-			startActivity(new Intent(this, AboutActivity.class));
-			return true;
+			case R.id.about:
+				startActivity(new Intent(this, AboutActivity.class));
+				return true;
 
-		default:
-			return super.onOptionsItemSelected(item);
+			default:
+				return super.onOptionsItemSelected(item);
 		}
 
 	}
@@ -78,24 +85,7 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		if (v == mLoginButton) {
-			Memory.setString(this, Constant.MEMORY_KEY_USER, mUsernameEditText
-					.getText().toString());
-			Memory.setString(this, Constant.MEMORY_KEY_PASSWORD,
-					mPasswordEditText.getText().toString());
-			LoginHelper.login(this, mUsernameEditText.getText().toString(),
-					mPasswordEditText.getText().toString(),
-					new GeneralCallback() {
-
-						@Override
-						public void onSuccess() {
-							showMessage(R.string.login_sucessful);
-						}
-
-						@Override
-						public void onFail(String reason) {
-							showMessage(reason);
-						}
-					});
+			saveAndLogin();
 		} else if (v == mLogoutButton) {
 			LoginHelper.logout(this, new GeneralCallback() {
 
@@ -110,6 +100,25 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 				}
 			});
 		}
+	}
+
+	private void saveAndLogin() {
+		Memory.setString(this, Constant.MEMORY_KEY_USER, mUsernameEditText.getText().toString());
+		Memory.setString(this, Constant.MEMORY_KEY_PASSWORD,
+				mPasswordEditText.getText().toString());
+		LoginHelper.login(this, mUsernameEditText.getText().toString(),
+				mPasswordEditText.getText().toString(), new GeneralCallback() {
+
+					@Override
+					public void onSuccess() {
+						showMessage(R.string.login_sucessful);
+					}
+
+					@Override
+					public void onFail(String reason) {
+						showMessage(reason);
+					}
+				});
 	}
 
 	private void showMessage(int messageRes) {
