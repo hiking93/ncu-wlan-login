4dc28e6ccc6905d39afaa3ccdebdbbf591cd2bb3
Hiking <tom.xj.cheng@gmail.com>
2015-02-27 17:41:59 +0800

    Switch to Android Studio, mipmap launcher icons, TANetRoaming support.

---
+++ NCUWLogin/app/src/main/java/com/devandroid/ncuwlogin/callbacks/Constant.java
@@ -0,0 +1,14 @@
+package com.devandroid.ncuwlogin.callbacks;
+
+public class Constant {
+
+	public static final String[] EXPECTED_SSIDS = {"NCUWL", "TANetRoaming"};
+	public static final String TAG = "Hiking";
+
+	// Notification IDs
+	public static final int NOTIFICATION_LOGIN_ID = 100;
+
+	// Memory (Shared Preferences) Keys
+	public static final String MEMORY_KEY_USER = "MEMORY_KEY_USER";
+	public static final String MEMORY_KEY_PASSWORD = "MEMORY_KEY_PASSWORD";
+}
