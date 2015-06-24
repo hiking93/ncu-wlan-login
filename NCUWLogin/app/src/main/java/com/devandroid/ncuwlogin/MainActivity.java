5d6568b07203736b56b08e82a9777124687fe091
Hiking <tom.xj.cheng@gmail.com>
2015-06-24 17:01:31 +0800

    Update launcher icons

--- NCUWLogin/app/src/main/java/com/devandroid/ncuwlogin/MainActivity.java
+++ NCUWLogin/app/src/main/java/com/devandroid/ncuwlogin/MainActivity.java
@@ -2,7 +2,7 @@ package com.devandroid.ncuwlogin;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,7 +20,7 @@ import com.devandroid.ncuwlogin.libs.LoginHelper;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class MainActivity extends ActionBarActivity implements OnClickListener {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
 	@InjectView(R.id.button_login)
 	Button mLoginButton;
